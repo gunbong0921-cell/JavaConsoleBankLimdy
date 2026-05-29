39a0a7f7032f7e3ab493aad9d7a517045d708981
gunbong0921 <gunbong0921@gmail.com>
2026-05-29 10:51:20 +0900

    순번적용함

--- src/banking/step1/BankingSystemMain.java
+++ src/banking/step1/BankingSystemMain.java
@@ -3,9 +3,9 @@ package banking.step1;
 import java.util.Scanner;
 
 public class BankingSystemMain {
-	//1
-	//2-1
-	//2-2
+	//1.1
+	//2.2-1
+	//3.2-2
     public static void main(String[] args) {
     	
         Scanner sc = new Scanner(System.in);
